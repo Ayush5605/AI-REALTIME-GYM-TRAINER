d2374962e4909d36ae64ab1dedf57d2e64a8b7ea
Ayush Auti <ayushauti5605@gmail.com>
2026-08-16 14:25:10 +0530

    added the exercise table in db

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from services.auth.login_wall import render_login_wall
 from services.state.session_default import initial_session_defaults
 from services.config.workout_config import EXERCISE_OPTIONS
 from services.ui.style_loader import load_css,inject_local_font
+from services.persistence.exercise_repository import init_db
 
 
 def main():
@@ -18,6 +19,8 @@ def main():
 
     load_css(os.path.join(os.getcwd(),"static","style.css"))
     inject_local_font(os.path.join(os.getcwd(),"static","AdobeClean.otf"),"AdobeClean")
+
+    init_db()
 
     # Initialize session state
     initial_session_defaults()
